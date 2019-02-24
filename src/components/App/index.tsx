df139f9aaf5c5acf8379034a1bd5df55217d447a
qsat <araisat@gmail.com>
2019-02-25 02:09:18 +0900

    check dnd

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Nav from '../Nav/index.tsx'
+import DragDrop from '../DragDrop/index.tsx'
+
+window.addEventListener('scroll', () => console.log('aaaa')) // not work
 
 export default function App(): React.ComponentElement<{}, any> { 
   return (
-    <div>
+    <div
+      onMouseMove={() => console.log('mousemove')} // work
+      onScroll={() => console.log('scroll')} // not work
+    >
       <h1 key='map'>Precinct Map</h1>
       <div draggable>aaa</div>
       <Nav />
+      <DragDrop />
     </div>
   )
 } 
